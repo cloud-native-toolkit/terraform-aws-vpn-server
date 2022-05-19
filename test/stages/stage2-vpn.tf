2b63aa347d80cc1550f49427b04c573fad8dca6f
faheemshai <fahim.shaikh@ibm.com>
2022-05-19 17:45:58 +0530

    latest
    Signed-off-by: faheemshai <fahim.shaikh@ibm.com>

--- test/stages/stage2-vpn.tf
+++ test/stages/stage2-vpn.tf
@@ -2,16 +2,14 @@ module "dev_vpn" {
 source    = "./module"
 depends_on                  = [module.subnets]
 client_cidr_block           = var.client_cidr_block
-allowed_cidr_ranges         = [var.internal_cidr_pub, var.internal_cidr_dev]
-//route                       = var.route
-//active_directory_id         = ""
+allowed_cidr_ranges         = [var.internal_cidr, var.internal_cidr_dev]
 name                        = var.name
 name_prefix                 = var.name_prefix
 vpc_id                      = module.vpc.vpc_id
-/*cloudwatch_log_group        = "client_vpn_endpoint"*/
 region                      = var.region
-subnet_ids                 = module.subnets_private.subnet_ids
-dns_servers                = var.dns_servers
-number_subnets_vpn         = length(var.subnet_private_cidrs)
-logs_retention             = var.logs_retention
+subnet_ids                  = module.subnets_private.subnet_ids
+dns_servers                 = var.dns_servers
+number_subnets_association  = length(var.subnet_private_cidrs)
+additional_routes           =  ["10.1.0.0/16", "10.2.0.0/16"] 
+number_additional_routes    = 4      
 }
--- variables.tf
+++ variables.tf
@@ -1,26 +1,25 @@
 variable "region" {
   type = string
   description = "The deployment region"
-  default = "ap-south-1"
+  default = ""
 }
-
 
 variable "subnet_ids" {
   type = list(string)
-  description = "Subnet ID to associate clients (each subnet passed will create an VPN association - costs involved)"
-  default = []
-}
-
-variable "route_subnet_ids" {
-  type = list(string)
-  description = "Subnet ID to associate additonal route for clients (each subnet passed will create a VPN route )"
+  description = "Subnet ID to associate clients (each subnet passed will create an VPN association -costs involved)"
   default = []
 }
 
 variable "additional_routes" {
-  type = list(string)
-  description = "Subnet ID to associate additonal route for clients (each subnet passed will create a VPN route )"
-  default = []
+  type        = list(string)
+  default     = []
+  description = "List of additonal routes to add in VPN"
+}
+
+variable "number_additional_routes" {
+  type        = number
+  default     = 0
+  description = "cont of additonal routes to add in VPN"
 }
 
 variable "allowed_cidr_ranges" {
@@ -35,12 +34,22 @@ variable "vpc_id" {
    default = ""
 }
 
+variable "create_vpn" {
+  type        = bool
+  default     = true
+  description = "Indicates whether you want to  create vpn"
+}
+
+variable "existing_vpn_id" {
+  type = string
+   description = "VPC Id to create resources"
+   default = ""
+}
 variable "security_group_id" {
   type        = string
   description = "Optional security group id to use instead of the default created"
   default     = ""
 }
-
 
 variable "client_cidr_block" {
    type = string
@@ -73,6 +82,7 @@ variable "authentication_saml_provider_arn" {
   default     = null
   description = "(Optional) The ARN of the IAM SAML identity provider if type is federated-authentication."
 }
+
 variable "resource_group_name" {
   type        = string
   description = "The name of the resource group where the VPC is deployed. On AWS this value becomes a tag."
@@ -84,8 +94,8 @@ variable "split_tunnel" {
   description = "With split_tunnel false, all client traffic will go through the VPN."
 }
 
-variable "number_subnets_vpn" {
-   description = "list if subnets to attch with vpn"
+variable "number_subnets_association" {
+   description = "list of subnets to attch with vpn"
    type = number
    default = 0
 }
