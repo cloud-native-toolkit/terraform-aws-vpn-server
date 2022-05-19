81fcbdd230908a1078d6390173cf5fe2bd76239b
faheemshai <fahim.shaikh@ibm.com>
2022-05-19 18:40:08 +0530

    latest
    Signed-off-by: faheemshai <fahim.shaikh@ibm.com>

--- test/stages/stage2-vpn.tf
+++ test/stages/stage2-vpn.tf
@@ -2,7 +2,7 @@ module "dev_vpn" {
 source    = "./module"
 depends_on                  = [module.subnets]
 client_cidr_block           = var.client_cidr_block
-allowed_cidr_ranges         = [var.internal_cidr, var.internal_cidr_dev]
+allowed_cidr_ranges         = [var.internal_cidr_edge, var.internal_cidr_dev]
 name                        = var.name
 name_prefix                 = var.name_prefix
 vpc_id                      = module.vpc.vpc_id
