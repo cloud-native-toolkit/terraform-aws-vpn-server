ad4e1e1ce9f29b56ddda9349fa9f70b89421812f
faheemshai <fahim.shaikh@ibm.com>
2022-07-19 18:35:05 +0530

    cloudwatch update
    Signed-off-by: faheemshai <fahim.shaikh@ibm.com>

--- test/stages/stage2-vpn.tf
+++ test/stages/stage2-vpn.tf
@@ -3,7 +3,6 @@ source    = "./module"
 depends_on                  = [module.subnets]
 client_cidr_block           = var.client_cidr_block
 allowed_cidr_ranges         = [var.internal_cidr_edge, var.internal_cidr_dev]
-name                        = var.name
 name_prefix                 = var.name_prefix
 vpc_id                      = module.vpc.vpc_id
 region                      = var.region_vpn
