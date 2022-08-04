aaaca1b26d700555b7c578ebafe9348495586695
faheemshai <fahim.shaikh@ibm.com>
2022-08-04 16:59:00 +0530

    nameserver
    Signed-off-by: faheemshai <fahim.shaikh@ibm.com>

--- scripts/generate-vpnprofile.sh
+++ scripts/generate-vpnprofile.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # This script generates ovpn profile for client vpn
 vpn_endpoint_id=${VPN_ID}
-
 echo "generate vpn client config file ..."
 aws ec2 export-client-vpn-client-configuration \
     --client-vpn-endpoint-id $vpn_endpoint_id --region ${REGION}  \
@@ -13,5 +12,7 @@ echo "</cert>"                     >>  ${VPN_SERVER}.ovpn
 echo "<key>"                       >>  ${VPN_SERVER}.ovpn
 cat ./certificates/private/client1.vpn.aws.com.key >>  ${VPN_SERVER}.ovpn
 echo "</key>"                      >>  ${VPN_SERVER}.ovpn
-
 echo "Your OpenVPN client profile has been created, with certificates added, and is available at $(pwd)/${VPN_SERVER}.ovpn"
+
+echo "generate nameserver file ..."
+aws ec2 describe-client-vpn-endpoints  --client-vpn-endpoint-id $vpn_endpoint_id --region ${REGION} --output text | grep DNSSERVERS | awk  '{print $2}' >  nameserver
